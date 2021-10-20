12b864496bdcdb12c1bb2d6e78efbe46fcacf71c
sebastianRellihan <srellihan@gmail.com>
2021-10-20 00:33:16 -0300

    Formularios de Login & Registro

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -1,12 +1,71 @@
 import React, { Component } from 'react'
-import { Text, StyleSheet } from 'react-native'
+import {View, Text, TextInput, StyleSheet, TouchableOpacity} from 'react-native';
 
-export default class Register extends Component {
+export default class Login extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            email:'',
+            password:'',
+            error:'',
+        }
+    }
+
+    onSubmit(){
+        console.log(`El email ingresado es: ${this.state.email}`);
+        console.log(`La contraseña ingresada es: ${this.state.password}`);
+    }
+
     render() {
         return (
-            <Text>Register</Text>
+            <View style={styles.formContainer}>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={(text)=>this.setState({email: text})}
+                    placeholder='email'
+                    keyboardType='email-address'/>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={(text)=>this.setState({password: text})}
+                    placeholder='password'
+                    keyboardType='email-address'
+                    secureTextEntry={true}
+                />
+                <TouchableOpacity style={styles.button} onPress={() => this.onSubmit()}>
+                    <Text style={styles.textButton}>Registrarse</Text>    
+                </TouchableOpacity>
+            </View>
         )
     }
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    formContainer:{
+        paddingHorizontal:10,
+        marginTop: 20,
+    },
+    input:{
+        height:20,
+        paddingVertical:15,
+        paddingHorizontal: 10,
+        borderWidth:1,
+        borderColor: '#ccc',
+        borderStyle: 'solid',
+        borderRadius: 6,
+        marginVertical:10,
+    },
+    button:{
+        backgroundColor:'#28a745',
+        paddingHorizontal: 10,
+        paddingVertical: 6,
+        textAlign: 'center',
+        borderRadius:4, 
+        borderWidth:1,
+        borderStyle: 'solid',
+        borderColor: '#28a745'
+    },
+    textButton:{
+        color: '#fff'
+    }
+
+})
